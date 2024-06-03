4bdc417730489a1ab130c6db807e57b884436058
ntxsay <ntxsay@hotmail.fr>
2024-06-03 04:53:43 -0400

    Mise en forme page 404

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -6,7 +6,7 @@ const NotFoundPage = () =>{
     return (
         <section id="notFoundPage" className="page">
             <p className="page__error-code">404</p>
-            <p className="page__message">Oups! La page que vous demandez n'existe pas.</p>
+            <p className="page__message"><span>Oups! La page que </span><span>vous demandez n'existe pas.</span></p>
             <a href="/" className="page__return-link">Retourner sur la page d’accueil</a>
         </section>
     );
